4c3f1f3d7a49e6d156f01981ab3b1ff3b7cc5271
khalvat-M <mmba313@gmail.com>
2025-07-23 05:43:21 +0330

    sm8250-common: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    # Conflicts:
    #	proprietary/system_ext/bin/wfdservice

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -263,6 +263,8 @@ PRODUCT_PACKAGES += \
     libshoebox \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
+    vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     android.hardware.secure_element@1.0-impl \
     com.qualcomm.qti.dpm.api@1.0_vendor \
     com.qualcomm.qti.imscmservice@1.0 \
@@ -522,7 +524,6 @@ PRODUCT_PACKAGES += \
     capi_v2_aptX_CLHDADV_Encoder \
     capi_v2_aptX_CLHDAD_Speech_Decoder \
     com.qualcomm.qti.dpm.api@1.0 \
-    com.qualcomm.qti.wifidisplayhal@1.0 \
     lib-imsvideocodec \
     lib-imsvt \
     lib-imsvtextutils \
@@ -642,7 +643,7 @@ PRODUCT_PACKAGES += \
     xtwifi-client \
     xtwifi-inet-agent \
     dpmd \
-    wfdservice64
+    wfdservice
 
 PRODUCT_PACKAGES += \
     vendor_lib_libEGL_adreno_so \
